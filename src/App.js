5e15e4f870f262a7cfc45680340b71322b7bd691
JeremieG06300 <jeremie.garod@epitech.eu>
2023-03-25 19:36:56 +0100

    add gauge.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-// import Gauge from "./Gauge";
-
+import logo from "./logo.svg";
+import "./App.css";
+import Gauge from "./components/Gauge";
 // import Component2 from './components/views/composant2';
 // import Composant from './components/views/composant';
 
 function App() {
   return (
     <div className="App">
+      <h1>OGHJI</h1>
+     <Gauge/>
 
-<h1>OGHJI</h1>
-{/* 
-<Gauge/> */}
-
-<div>
-{/*      
+      <div>
+        {/*      
     <Composant/> */}
-    </div>
-
+      </div>
     </div>
   );
 }
