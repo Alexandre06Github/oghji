348b6e1c9e32c72be30c8c32a4aa1a5f18c53c76
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-03-26 13:43:27 +0200

    Updates SideBar & Home.js

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,9 +1,8 @@
 import Composant from "./composant";
-import "../assets/style/styleAlexandre.css"
+import "../assets/style/styleAlexandre.css";
 import Menu from "../assets/menu";
 import Button from "../button";
 import Gauge from "../Gauge";
-
 
 import IconTelevision from "../assets/icons/tv";
 import IconBxFridge from "../assets/icons/kitchen";
@@ -12,46 +11,41 @@ import IconBedOutline from "../assets/icons/room";
 import IconAddFill from "../assets/icons/add";
 
 export default function Home() {
-
   return (
-  
     <div>
-      
       <Menu />
-      <Button/>
-
       <div className="room">
-      <Button
-            name="Salon"
-            icon={<IconTelevision />}
-            onClick={() => console.log("Salon button clicked")}
-          />
-      <Button
-            name="Cuisine"
-            icon={<IconBxFridge />}
-            onClick={() => console.log("Salon button clicked")}
-          />
-      
-      <Button
-            name="Sdb"
-            icon={<IconBath />}
-            onClick={() => console.log("Salon button clicked")}
-          />
-    
-      <Button
-            name="Chambre"
-            icon={<IconBedOutline />}
-            onClick={() => console.log("Salon button clicked")}
-          />
-      
+        <Button
+          name="Salon"
+          icon={<IconTelevision />}
+          onClick={() => console.log("Salon button clicked")}
+        />
+        <Button
+          name="Cuisine"
+          icon={<IconBxFridge />}
+          onClick={() => console.log("Salon button clicked")}
+        />
+
+        <Button
+          name="Sdb"
+          icon={<IconBath />}
+          onClick={() => console.log("Salon button clicked")}
+        />
+
+        <Button
+          name="Chambre"
+          icon={<IconBedOutline />}
+          onClick={() => console.log("Salon button clicked")}
+        />
       </div>
-      <div className="add"><Button
-            name="Ajouter"
-            icon={<IconAddFill />}
-            onClick={() => console.log("Salon button clicked")}
-          /></div>
+      <div className="add">
+        <Button
+          name="Ajouter"
+          icon={<IconAddFill />}
+          onClick={() => console.log("Salon button clicked")}
+        />
+      </div>
       <Composant />
-      <Gauge/>
     </div>
   );
 }
